5cd3628254cfc00e015d3da1d18e639ba220ecde
su-su-su-su <suzukikoji0503@gmail.com>
2024-04-08 15:38:47 +0900

    rubocopに対しての修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -9,16 +9,16 @@ class Blog < ApplicationRecord
 
   scope :published, -> { where('secret = FALSE') }
 
-  scope :search, ->(term) {
-    term = term.nil? ? "" : term
-    where("title LIKE ? OR content LIKE ?", 
-          "%" + ActiveRecord::Base.sanitize_sql_like(term) + "%",
-          "%" + ActiveRecord::Base.sanitize_sql_like(term) + "%")
-  }  
+  scope :search, lambda { |term|
+    term = term.nil? ? '' : term
+    where('title LIKE ? OR content LIKE ?',
+          "%#{ActiveRecord::Base.sanitize_sql_like(term)}%",
+          "%#{ActiveRecord::Base.sanitize_sql_like(term)}%")
+  }
 
   scope :default_order, -> { order(id: :desc) }
 
-  scope :accessible_blogs, ->(user) { where(user: user).or(where(secret: false)) }
+  scope :accessible_blogs, ->(user) { where(user:).or(where(secret: false)) }
 
   def owned_by?(target_user)
     user == target_user
